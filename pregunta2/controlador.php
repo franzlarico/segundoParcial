50bac10dcafdddacef79bf517121283460c05ffe
franzlarico <73504974+franzlarico@users.noreply.github.com>
2021-05-30 23:54:22 -0400

    Add files via upload

---
+++ pregunta2/controlador.php
@@ -0,0 +1,27 @@
+<?php
+include "conexion.inc.php";
+$codflujo = $_GET["codflujo"];
+$codproceso = $_GET["codproceso"];
+$pantalla = "controlador.".$_GET["pantalla"];
+include $pantalla;
+
+if(isset($_GET["Siguiente"])){
+	$sql ="select * from proceso where codFlujo='$codflujo' ";
+	$sql.="and codProceso='$codproceso' ";
+	$resultado = mysqli_query($con, $sql);
+	$fila = mysqli_fetch_row($resultado);
+	$cf = $fila[0];
+	$cp = $fila[2];
+
+}
+if(isset($_GET["Anterior"])){
+	$sql ="select * from proceso ";
+	$sql.="where codFlujo='$codflujo' ";
+	$sql.="and codProcesoSiguiente='$codproceso' ";	
+	$resultado = mysqli_query($con, $sql);
+	$fila = mysqli_fetch_row($resultado);
+	$cf = $fila[0];
+	$cp = $fila[1];	
+}
+header("Location: motor.php?cf=$cf&cp=$cp");
+?>
---
+++ pregunta2/footer.php
@@ -0,0 +1,20 @@
+
+
+<div style="font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif; 
+	color: #ffffff; 
+	font-size: 18px; 
+	font-weight: 400; 
+	text-align: center; 
+	background: gray;  
+	overflow: hidden;   
+	margin-top:5%;
+	width:100%;
+	height: 15	%;
+">		<h2><FONT COLOR="white">
+		Monoblock central - Segundo Patio - Telefono 2440338
+		</h2></FONT>
+</div>
+
+    
+</body>
+</html>
